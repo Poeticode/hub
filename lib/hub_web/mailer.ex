c9805744059c54932c19029306a1c53d6955920c
Poeticode <silas@poeti.codes>
2019-03-14 23:48:13 -0400

    adjust url in sent email

--- lib/hub_web/mailer.ex
+++ lib/hub_web/mailer.ex
@@ -11,14 +11,14 @@ defmodule Hub.Email do
 		base_email()
 		|> to(recipient)
 		|> with_template(Application.get_env(:hub, :sendgrid_template_id))
-		|> add_dynamic_field(:url, "localhost:4000/admin/posts/#{post_id}")
+		|> add_dynamic_field(:url, "https://chattanoogapoetscollective/login")
 		|> add_dynamic_field(:title, title)
 		|> add_dynamic_field(:author, author)
 	end
 
 	defp base_email do
     new_email
-    |> from("Collective <me@silentsilas.com>")
+    |> from("Chattanooga Poet's Collective <me@silentsilas.com>")
     # This will use the "email.html.eex" file as a layout when rendering html emails.
     # Plain text emails will not use a layout unless you use `put_text_layout`
     # |> put_html_layout({Myapp.LayoutView, "email.html"})
