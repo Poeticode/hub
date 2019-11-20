c1990ba8b8911a0654423b288070dc07f8caf691
Silas Tippens <silas@frayt.com>
2019-11-20 12:06:43 -0500

    add thumbnail version to uploaded avatars, and use these smaller files on site

--- lib/hub_web/uploaders/avatar.ex
+++ lib/hub_web/uploaders/avatar.ex
@@ -28,9 +28,9 @@ defmodule Hub.Avatar do
   end
 
   # Override the persisted filenames:
-  # def filename(version, _) do
-	# 	version
-  # end
+  def filename(version, _) do
+		version
+  end
 
   # Override the storage directory:
   def storage_dir(version, {file, scope}) do
